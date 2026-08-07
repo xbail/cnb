50691c2a5544bb087a3b39129536e2940212721a
小白龙 <81628469+xbail@users.noreply.github.com>
2026-08-07 18:41:27 +0800

    fix: restore proxy upload (CORS), rename to CNB ???, fix audio Content-Type

--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -29,7 +29,7 @@ const isAlbum = () => route.path === '/album'
               d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
           </svg>
         </div>
-        <span class="font-bold text-xl gradient-text">CNB 图床</span>
+        <span class="font-bold text-xl gradient-text">CNB 小网盘</span>
       </div>
 
       <div class="flex items-center gap-2">
